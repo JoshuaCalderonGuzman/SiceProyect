4bd00d68c68b66abfb33f14fb6bee6144d2fbd13
RobeGGs111 <s22120166@alumnos.itsur.edu.mx>
2026-02-03 23:29:30 -0600

    Cambio de la paleta de colores

--- app/src/main/java/com/example/siceproyect/ui/theme/Color.kt
+++ app/src/main/java/com/example/siceproyect/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.siceproyect.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Paleta extraída de tu imagen
+val Terracotta = Color(0xFFF07F5B)
+val OrangeGold = Color(0xFFF0B35B)
+val DeepOrange = Color(0xFFF0995B)
+val CoralRed = Color(0xFFF0655B)
+val GoldenYellow = Color(0xFFF0C55B)
+val SoftPeach = Color(0xFFEFC2A1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Colores adicionales para contraste
+val CharcoalGray = Color(0xFF1C1B1F)
+val OffWhite = Color(0xFFFFFBFE)
